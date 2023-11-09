378638b06954abcb976a0f1b8fa1b5babf049ec1
JaoCorrea <128312734+JaoCorrea@users.noreply.github.com>
2023-11-09 08:36:47 -0300

    Add files via upload

---
+++ la pepekada/script.js
@@ -0,0 +1,30 @@
+function potencia() {
+    let n = 5;
+    let pote = n;
+
+    while (n>=1) {
+            pote = Math.pow(2, n);
+            n = n - 1;
+            alert (pote);
+        let n = parseInt(numer.value);
+        let fat= n ;
+        if (n<o){
+            alert("numero negativo");
+        }
+         else if (n == 0){
+            alerte ("1");
+         }
+
+         else {
+            while (n>1){
+                fat = fat *  (n -1)
+                    n = n -1;
+                
+            }
+         }
+    }
+
+    
+}
+
+
